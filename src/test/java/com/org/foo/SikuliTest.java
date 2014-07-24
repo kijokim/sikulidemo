cb2362c68e6684c3d80ae7ea5de6bd6607ed7857
kijokim <ki-jo.kim@accenture.com>
2014-07-24 15:09:59 +0900

    initial

---
+++ src/test/java/com/org/foo/SikuliTest.java
@@ -0,0 +1,43 @@
+package com.org.foo;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.sikuli.script.App;
+import org.sikuli.script.Pattern;
+import org.sikuli.script.Screen;
+
+/**
+ * Created by kijo.kim on 7/15/14.
+ */
+public class SikuliTest {
+
+	@Before
+	public void setup() {
+		// File file = new File("/Volumes/HD/work/tools/chromedriver");
+		// System.setProperty("webdriver.chrome.driver",file.getAbsolutePath());
+	}
+
+	@Test
+	public void testPOS() {
+		App app =new App("C:\\Program Files (x86)\\YG-POS\\YG_POS.exe");
+		try {
+			app.open();
+			Thread.sleep(1000);
+			Screen screen = new Screen();
+			screen.click(new Pattern("data\\1.png"));
+			screen.click(new Pattern("data\\2.png"));
+			Thread.sleep(1000);
+			screen.type(new Pattern("data\\3.png"), "11112222");
+			screen.click(new Pattern("data\\4.png"));
+			screen.find(new Pattern("data\\5.png"));
+
+		} catch (Exception e) {
+			e.printStackTrace();
+			Assert.fail();
+		}finally {
+			app.close();
+		}
+
+	}
+}
