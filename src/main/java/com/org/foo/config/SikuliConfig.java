82b9ce7c709d53bc80252aff9569c3de4eb7b31f
kijokim <ki-jo.kim@accenture.com>
2014-08-01 11:52:57 +0900

    Revert "update"
    This reverts commit ca67dbf11fb1758cd70aaebf97d8c53ebd6c6cae.

--- src/main/java/com/org/foo/config/SikuliConfig.java
+++ src/main/java/com/org/foo/config/SikuliConfig.java
@@ -1,31 +1,19 @@
 package com.org.foo.config;
 
-import org.sikuli.script.Screen;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import org.sikuli.script.Pattern;
 import org.springframework.beans.factory.InitializingBean;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
-import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
 import org.springframework.core.env.Environment;
 
-import com.org.foo.sikuli.CustomScreen;
-
 @Configuration
-@PropertySource(value = { "default.properties",
-		"classpath:${spring.profiles.active}.properties" }, ignoreResourceNotFound = true)
+@PropertySource("classpath:${spring.profiles.active:device1}.properties")
 public class SikuliConfig implements InitializingBean {
 
-	private static final Logger LOGGER = LoggerFactory
-			.getLogger(SikuliConfig.class);
 	@Autowired
 	private Environment env;
-
-	@Autowired
-	private ApplicationContext context;
 
 	private String targetDevice;
 
@@ -38,24 +26,16 @@ public class SikuliConfig implements InitializingBean {
 	}
 
 	@Bean
-	public static PropertySourcesPlaceholderConfigurer propertySourcesPlaceholderConfigurer() {
-		return new PropertySourcesPlaceholderConfigurer();
-	}
-
-	@Bean
-	public Screen customScreen() {
-		return new CustomScreen("src/main/resources/" + getTargetDevice()
+	public Pattern customPattern() {
+		return new CustomPattern("src/main/resources/" + getTargetDevice()
 				+ "/");
 	}
-	
+
 	public void afterPropertiesSet() throws Exception {
 		if (env.getActiveProfiles().length == 0) {
 			throw new Exception("avtive Profile does not exist!!!");
 		} else {
 			setTargetDevice(env.getActiveProfiles()[0]);
 		}
-		System.out.println("target deivce [" + getTargetDevice()
-				+ "] is selected!");
-
 	}
 }
--- src/test/java/com/org/foo/SikuliResourceChangeTest.java
+++ src/test/java/com/org/foo/SikuliResourceChangeTest.java
@@ -1,25 +1,60 @@
 package com.org.foo;
 
 import org.junit.Assert;
+import org.junit.BeforeClass;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.sikuli.script.App;
+import org.sikuli.script.Pattern;
+import org.sikuli.script.Screen;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.support.AnnotationConfigContextLoader;
+
+import com.org.foo.config.SikuliConfig;
 
 
-public class SikuliResourceChangeTest extends SikuliTestCase {
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes=SikuliConfig.class, loader=AnnotationConfigContextLoader.class)
+public class SikuliResourceChangeTest {
+
+	@Autowired
+	Pattern p;
+	
+	@Autowired
+	private Environment env;
+
+	
+	@BeforeClass
+	public static void setupOnce() {
+		System.setProperty("spring.profiles.active", "device2");
+	}
 
 	@Test
 	public void testPOS() {
-		// comment
+		//comment
+		App app =new App("C:\\Program Files (x86)\\YG-POS\\YG_POS.exe");
 		try {
 			app.open();
-			screen.click(key1);
-			screen.click(key2);
-			screen.type(key3, "1111");
-			screen.click(key4);
-			screen.find(key5);
+			Thread.sleep(1000);
+			Screen screen = new Screen();
+			screen.click(p.setFilename(env.getProperty("key1")));
+			screen.click(p.setFilename(env.getProperty("key2")));
+			Thread.sleep(1000);
+			screen.type(p.setFilename(env.getProperty("key3")),"1111");
+			screen.click(p.setFilename(env.getProperty("key4")));
+			screen.find(p.setFilename(env.getProperty("key5")));
+
 
 		} catch (Exception e) {
 			e.printStackTrace();
 			Assert.fail();
-		} 
+		}finally {
+			app.close();
+		}
+
 	}
+
 }
