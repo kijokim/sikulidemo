6974b0ebcde1b4c32d79c821aca339ba68ed57ba
kijo.kim <hex2023@gmail.com>
2014-08-06 14:53:59 +0900

    update testng resources

--- src/test/java/com/gs/pos/testcase/Scenario1ResourceChangeTest.java
+++ src/test/java/com/gs/pos/testcase/Scenario1ResourceChangeTest.java
@@ -14,8 +14,35 @@ public class Scenario1ResourceChangeTest extends Scenario1TestHelper {
 
     }
 
-    @Test
+    @Test(groups = {"ygpos.validation1"})
     public void testPOS() {
+        try {
+            app.open();
+            screen.click(key1);
+            screen.click(key2);
+            screen.type(key3, "1111");
+            screen.click(key4);
+            screen.find(key5);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            Assert.fail();
+        }
+    }
+
+    @Test(groups = {"ygpos.validation1"})
+    public void testPOS1() {
+        try {
+            System.out.println("\"SSS\" = " + "SSS");
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            Assert.fail();
+        }
+    }
+
+    @Test(enabled = false)
+    public void testPOS2() {
         try {
             app.open();
             screen.click(key1);
